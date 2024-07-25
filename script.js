7fda9b72b4995f89787dc5bc02f1bd1b4b5ea018
amal <121586027+AmalKrishnaS@users.noreply.github.com>
2024-07-25 14:13:42 +0530

    Create variables for storing user input
    - Created variable for firstNumber
    - Created variable for secondNumber
    - Created variable for operator

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ function multiply (firstNumber, secondNumber) {
 function divide (firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
+
+const firstNumber = 0;
+const secondNumber = 0;
+const operator = '';

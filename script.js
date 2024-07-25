d209e21846dea1f5e5949e4756620b81239a004b
amal <121586027+AmalKrishnaS@users.noreply.github.com>
2024-07-25 14:26:54 +0530

    Create operate function - Added function to invoke arithmetic operation based on input operator - Renamed divide function to division

--- script.js
+++ script.js
@@ -10,10 +10,24 @@ function multiply (firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
 
-function divide (firstNumber, secondNumber) {
+function division (firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
 
 const firstNumber = 0;
 const secondNumber = 0;
 const operator = '';
+
+function operate (firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return substract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return division(firstNumber, secondNumber);
+    }
+
+}
